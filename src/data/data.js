4cd1cc2b25a7a0c6143fba1f56623ac96d79347f
DavidSaperstein <d.h.saperstein@gmail.com>
2020-11-12 14:25:24 -0700

    added data

---
+++ src/data/data.js
@@ -0,0 +1,61 @@
+export const projectsData = [
+  {
+    id: 1,
+    title: 'Magic Draft Reference',
+    description: '',
+    githubLink: '',
+    deployedLink: '',
+    stack: [],
+    
+  }
+]
+
+export const iconsThanks = [
+  {
+    id: 1,
+    title: 'mongoDB',
+    thanks: '<a href="https://iconscout.com/icons/mongodb" target="_blank">Mongodb Icon</a> by <a href="https://iconscout.com/contributors/icon-mafia">Icon Mafia</a> on <a href="https://iconscout.com">Iconscout</a>'
+  },
+
+  {
+    id: 2,
+    title: 'React',
+    thanks: '<a href="https://iconscout.com/icons/react" target="_blank">React Icon</a> by <a href="https://iconscout.com/contributors/icon-mafia" target="_blank">Icon Mafia</a>'
+  },
+  {
+    id: 3,
+    title: 'Express',
+    thanks: '<a href="https://iconscout.com/icons/react" target="_blank">React Icon</a> by <a href="https://iconscout.com/contributors/icon-mafia" target="_blank">Icon Mafia</a>'
+  },
+  {
+    id: 3,
+    title: 'Linkedin',
+    thanks: '<a href="https://iconscout.com/icons/linkedin" target="_blank">Linkedin Icon</a> by <a href="https://iconscout.com/contributors/icon-mafia">Icon Mafia</a> on <a href="https://iconscout.com">Iconscout</a>'
+  },
+  {
+    id: 4,
+    title: 'Javascript',
+    thanks: '<a href="https://iconscout.com/icons/javascript" target="_blank">Javascript Icon</a> by <a href="https://iconscout.com/contributors/icon-mafia">Icon Mafia</a> on <a href="https://iconscout.com">Iconscout</a>'
+  },
+  {
+    id: 5,
+    title: 'nodeJS',
+    thanks: '<a href="https://iconscout.com/icons/node-js" target="_blank">Node Js Icon</a> by <a href="https://iconscout.com/contributors/icon-mafia" target="_blank">Icon Mafia</a>'
+  },
+  {
+    id: 6,
+    title: 'HTML',
+    thanks: '<a href="https://iconscout.com/icons/html5" target="_blank">Html5 Icon</a> by <a href="https://iconscout.com/contributors/icon-mafia" target="_blank">Icon Mafia</a>'
+  },
+  {
+    id: 7,
+    title: 'CSS',
+    thanks: '<a href="https://iconscout.com/icons/css3" target="_blank">Css3 Icon</a> by <a href="https://iconscout.com/contributors/icon-mafia" target="_blank">Icon Mafia</a>'
+  },
+  {
+    id: 8,
+    title: 'GitHub',
+    thanks: '<a href="https://iconscout.com/icons/github" target="_blank">Github Icon</a> by <a href="https://iconscout.com/contributors/icon-mafia" target="_blank">Icon Mafia</a>'
+  }
+
+]
