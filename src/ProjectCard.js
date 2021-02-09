677cd413689c7483383b7d6ace383d0e434252de
DavidSaperstein <d.h.saperstein@gmail.com>
2021-02-09 12:52:15 -0700

    fixing deploy issues and padding

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const ProjectCard = (props) => {
   return (
     <div className='projects'>
-      <a href={props.deployedLink} target="_blank">
+      <a href={props.deployedLink} target="_blank" rel='noreferrer'>
         <h2>{props.title}</h2>
       </a>
-      <a href={props.deployedLink} target="_blank">
+      <a href={props.deployedLink} target="_blank" rel='noreferrer'>
         <img className='screenshots' src={props.screenshots[0]} alt={props.title}/>
       </a>
       <p>{props.description}</p>
